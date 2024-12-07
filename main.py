b7d4af85119e50b8bd231f0d0ae0efc1dfd88626
Ayomikun Ojueromi <aojueromi@outlook.com>
2024-12-07 12:30:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import calculator
+
+# Perform arithmetic operations
+a = 10
+b = 5
+
+print(f"The sum of {a} and {b} is: {calculator.add(a, b)}")
+print(f"The difference between {a} and {b} is: {calculator.subtract(a, b)}")
+print(f"The product of {a} and {b} is: {calculator.multiply(a, b)}")
+print(f"The quotient of {a} divided by {b} is: {calculator.divide(a, b)}")
